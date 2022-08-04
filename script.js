fa55cacc22012c1584675d468df0af3a2705ce31
Granm <70445682+Granm@users.noreply.github.com>
2022-08-04 18:07:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function bandeira(pais) {
+    return '<img src="paises/'+pais+'.png" />';
+}
+
+Vue.component("pais", {
+    props:['bandeira','continente'],
+    template:'<p v-html="bandeira + continente"></p>'
+});
+    
+
+
+let app = new Vue({
+    el: '#app',
+    data:{
+        argentina: bandeira('argentina'),
+        brazil: bandeira('brazil'),
+        china: bandeira('china'),
+        uk: bandeira('uk'),
+        usa: bandeira('usa')
+          },
+    
+    });
